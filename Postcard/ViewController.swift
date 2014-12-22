8069402fa39f5bae34bf5d3bf0778d2a2e0525ed
Christian Schad <christianschad@CSC-MBP-2.home>
2014-12-22 13:21:26 +0100

    Kommentierung um commits zu testen

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Test zweiter commit to GitHub
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
